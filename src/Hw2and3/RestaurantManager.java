cf28a891f7a760f4f4297f3de4261d62c651092e
Pornpavee Seri-umnuoy <pornpavee.se@ku.th>
2017-11-11 23:02:51 +0700

    re-submit hw2and3

---
+++ src/Hw2and3/RestaurantManager.java
@@ -0,0 +1,150 @@
+package Hw2and3;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *  Reading menufile
+ *
+ * @author Pornpavee Seri-umnuoy
+ *
+ */
+
+public class RestaurantManager {
+
+    public String[] menuList;
+    public double[] unitPrice;
+    public int orderNumber = 0;
+    public DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss\n");
+    public Date date = new Date();
+
+
+    public void init() {
+
+        String file = "data/menu.txt";
+        ClassLoader loader = RestaurantManager.class.getClassLoader();
+        InputStream text = loader.getResourceAsStream(file);
+
+        if (text == null) {
+            System.out.println("Could not access file "+file);
+            return;
+        }
+
+        Scanner reader = new Scanner(text);
+        int countLine = 0;
+        ArrayList<String> menu = new ArrayList<String>( );
+        ArrayList<Double> price = new ArrayList<Double>( );
+
+        while (reader.hasNextLine()) {
+
+            String sCurrentLine = reader.nextLine();
+            countLine++;
+            if (checkMenu(sCurrentLine)) {
+                String[] item = sCurrentLine.split(";");
+
+                double menuPrice;
+                try {
+                    menuPrice = Double.parseDouble(item[1]);
+                }catch (NumberFormatException ex){
+                    System.out.printf("%s error:%d",file,countLine);
+                    return;
+                }
+
+                menu.add(item[0]);
+                price.add(menuPrice);
+            }
+        }
+        reader.close();
+
+        menuList = new String[menu.size()];
+        unitPrice = new double[price.size()];
+
+        menu.toArray(menuList);
+        for(int k=0; k<unitPrice.length; k++) unitPrice[k] = price.get(k);
+    }
+
+    public String[] getMenuItems(){
+        return menuList;
+    }
+
+    public double[] getPrices(){
+        return unitPrice;
+    }
+
+
+
+    public static boolean checkMenu (String arg){
+
+        if (!arg.equals("")) {
+            if (arg.charAt(0) == '#') {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * for send the order to the chef
+     *
+     * orderNumber ref. receipt no.
+     * @param item customer's order
+     * @param quantity customer's order
+     */
+    public void recordOrder(ArrayList<String> item,ArrayList<Integer> quantity,double total){
+
+        try {
+            File file = new File("src/data/order.txt");
+            FileWriter fw = new FileWriter(file,true);
+            runOrderNumber();
+            fw.write(String.format("\n#%05d\n",orderNumber));
+            fw.write(dateFormat.format(date));
+           for(int i = 0; i < item.size(); i++)fw.write(String.format("%-20s %d\n",item.get(i),quantity.get(i)));
+
+            fw.write(String.format("\nTotal: %.2f\n",total));
+
+            fw.write("--------------------------");
+
+            fw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void runOrderNumber(){
+
+        String file = "data/order.txt";
+        ClassLoader loader = RestaurantManager.class.getClassLoader();
+        InputStream text = loader.getResourceAsStream(file);
+
+        Scanner reader = new Scanner(text);
+
+
+        while (reader.hasNextLine()) {
+            String sCurrentLine = reader.nextLine();
+            if (sCurrentLine.startsWith("#")){
+                orderNumber = Math.max(Integer.parseInt(sCurrentLine.substring(1).trim()),orderNumber);
+            }
+
+        }
+        reader.close();
+        orderNumber+=1;
+
+    }
+
+    public static void main(String[] args){
+
+        RestaurantManager rm = new RestaurantManager();
+        rm.init();
+
+        for(int k = 0; k < rm.menuList.length ; k++)System.out.printf("[%d] %-20.20s  %6.2f Baht.\n",k+1,rm.menuList[k], rm.unitPrice[k]);
+
+    }
+
+}
---
+++ src/Hw2and3/SkeRestaurant.java
@@ -0,0 +1,157 @@
+package Hw2and3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *  The main class of SKE Restaurant.
+ *  you can order with this class
+ *  quantity,price and items using an array
+ *
+ * @author Pornpavee Seri-umnuoy
+ *
+ */
+
+public class SkeRestaurant {
+
+   public static Scanner scan = new Scanner(System.in);
+    private static int[] quantity;
+    private static double[] prices;
+    private static String[] items;
+    public static RestaurantManager rm = new RestaurantManager();
+
+    private static void print(){
+
+        System.out.printf("%-21.20s  %10s  %12s Baht.\n","#Menu","Quantity","Price");
+        for(int k = 0; k < quantity.length ; k++){
+            if (quantity[k]>0){System.out.printf("%-21.20s  %10d  %12.2f Baht.\n", items[k],quantity[k],quantity[k]* prices[k]); }
+        }
+        double total = 0.0;
+        for(int k = 0; k < quantity.length; k++){ total += quantity[k]*prices[k];}
+        System.out.printf("\nTotal: %.2f Baht.\n",total);
+
+
+    }
+
+
+    /**
+     * change() for changing the quantity
+     * if you put a negative number
+     * this method will minus the quantity anyway
+     *
+     **/
+
+    private static void change() {
+
+        int countQuan = 0;
+        for (int n : quantity) countQuan += n;
+
+        if (countQuan == 0) System.out.println("Sorry, You didn't order yet.");
+        else for (int k = 0; k < quantity.length; k++)
+            if (quantity[k] > 0){ System.out.printf("\n[%d] %-20.20s", k + 1, items[k]);}
+
+        String change = UserInput("\nWhat menu do to want to Change? : ");
+        String numChange = UserInput("Remove: ");
+            if (isNumber(change) && isNumber(numChange) && Integer.parseInt(change) < items.length) {
+                quantity[Integer.parseInt(change) - 1] = Math.max(quantity[Integer.parseInt(change) - 1] - Integer.parseInt(numChange), 0);
+            } else System.out.println("Invalid input");
+
+    }
+
+    /**
+     * order() is the main method
+     * to order menu,print receipt and exit
+     * choice t,c and x
+     * work with small and capital letter
+     *
+     * minus quantity will be ignore
+     **/
+    private static void order(){
+        String choice,quan;
+        do {
+            choice = UserInput("\nEnter your choice: ");
+
+            if (isNumber(choice)) {
+                if(Integer.parseInt(choice) <= items.length && Integer.parseInt(choice) > 0) {
+                    quan = UserInput("Enter Quantity: ");
+                    while (!isNumber(quan)){
+                        System.out.println("\nInput invalid");
+                        quan = UserInput("Enter Quantity: ");
+                    }
+                    if (Integer.parseInt(quan) < 0) {
+                        quan = "0";
+                    }
+                    quantity[Integer.parseInt(choice) - 1] = quantity[Integer.parseInt(choice) - 1] + Integer.parseInt(quan);
+                }
+                else {
+                    System.out.printf("Sorry,but our restaurant have %d menus\n",items.length);
+                }
+            } else {
+                if(choice.equalsIgnoreCase("t")) print();
+                else if(choice.equalsIgnoreCase("c")) change();
+                else if(choice.equalsIgnoreCase("x")) checkOut();
+                else if(choice.equals("?")){allChoice();}
+                else if (choice.equals("-")) break;
+                else System.out.println("Enter ? for help");
+
+            }
+        }while (!choice.equalsIgnoreCase("-"));
+    }
+
+
+    private static String UserInput(String statement){
+        System.out.print(statement);
+        return scan.nextLine();
+    }
+
+    private static boolean isNumber(String word) {
+        try {
+            Integer.parseInt(word);
+            return true;
+        } catch(NumberFormatException ex) {
+            return false;
+        }
+    }
+    private static void checkOut(){
+        print();
+        System.out.println("Thank you for purchasing");
+
+        ArrayList<String> item = new ArrayList<>();
+        ArrayList<Integer> quan = new ArrayList<>();
+
+        for(int i = 0; i < quantity.length; i++) {
+            if (quantity[i] > 0){
+                item.add(items[i]);
+                quan.add(quantity[i]);
+            }
+        }
+
+        double total = 0.0;
+        for(int k = 0; k < quantity.length; k++){ total += quantity[k]*prices[k];}
+
+        rm.recordOrder(item,quan,total);
+
+        //delete the old order
+        quantity = new int[quantity.length];
+
+    }
+
+    private static void allChoice(){
+        for(int k = 0; k < items.length ; k++)System.out.printf("\n[%d] %-20.20s  %6.2f Baht.",k+1,items[k],prices[k]);
+        System.out.println("\n\n[t] Total\n[c] Change menu\n[x] Check out\n[-] Exit(without commit the current order)");
+    }
+
+
+    public static void main(String[] args) {
+
+        rm.init();
+
+        quantity = new int[rm.unitPrice.length];
+        items = rm.getMenuItems();
+        prices = rm.getPrices();
+
+        allChoice();
+        order();
+
+    }
+}
